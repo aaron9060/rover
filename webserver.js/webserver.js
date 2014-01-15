5d5b20944e16aadd9120d788920e29d93138920b
aaron <aaron9060@yahoo.com>
2014-01-14 23:50:49 -0600

    update4

--- webserver.js/webserver.js
+++ webserver.js/webserver.js
@@ -1,23 +1,28 @@
 // Serial Port Communications
-
+ 
 var com = require("serialport");
-
+ 
+function serialInit(){
 var serialPort = new com.SerialPort("/dev/ttyACM0", {
     baudrate: 9600,
     parser: com.parsers.readline('\r\n')
   });
-
+ 
 serialPort.on('open',function() {
   console.log('Port open');
 });
-
+ 
 serialPort.on('data', function(data) {
   console.log(data.toString());
 });
-
-
+ 
+console.log("CONSOLE PORT OPENED");
+}
+ 
+serialInit();
+ 
 // Serve Static Web Content (node-static)
-
+ 
 var http = require("http"),
     url = require("url"),
     path = require("path"),
@@ -28,7 +33,7 @@ var server = http.createServer(function(request, response) {
  
   var uri = url.parse(request.url).pathname
     , filename = path.join("/home/pi/dev/webserver.js/public", uri);
-  
+ 
   path.exists(filename, function(exists) {
     if(!exists) {
       response.writeHead(404, {"Content-Type": "text/plain"});
@@ -55,22 +60,22 @@ var server = http.createServer(function(request, response) {
 }).listen(parseInt(port, 10));
  
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
-
+ 
 // real-time communication (socket.io)
-
+ 
 function sleep(milliseconds) {
-	  var start = new Date().getTime();
-	  for (var i = 0; i < 1e7; i++) {
-	    if ((new Date().getTime() - start) > milliseconds){
-	      break;
-	    }
-	  }
-	}
-
-
-
+          var start = new Date().getTime();
+          for (var i = 0; i < 1e7; i++) {
+            if ((new Date().getTime() - start) > milliseconds){
+              break;
+            }
+          }
+        }
+ 
+ 
+ 
 var io = require('socket.io').listen(server);
-
+ 
 io.sockets.on('connection', function (socket) {
   socket.emit('server_status', { message: 'Connected' });
   socket.on('client_status', function (client_status) {
@@ -78,31 +83,30 @@ io.sockets.on('connection', function (socket) {
   });
   socket.on('client_cmd',function(client_cmd) {
     // for (var key in client_cmd)  
-	if("MOVE" in client_cmd ){
-        console.log("Writing to Serial Port: " + Object.keys(client_cmd).map(function(key) 
+        if("MOVE" in client_cmd ){
+        console.log("Writing to Serial Port: " + Object.keys(client_cmd).map(function(key)
           {return key + " " + client_cmd[key];}).join(" "));
-        serialPort.write(Object.keys(client_cmd).map(function(key) 
+        serialPort.write(Object.keys(client_cmd).map(function(key)
           {return key + " " + client_cmd[key];}).join(" "));
-	}
-	else if("SKETCH" in client_cmd){
-		var sketch = Object.keys(client_cmd).map(function(key){return client_cmd[key];});
-		console.log("Sketch Recieved:");
-		console.log(sketch);
-		var fs = require('fs');
-		fs.writeFile("/tmp/test", sketch , function(err) {
-			if(err) {
-				console.log(err);
-		    } else {
-		        console.log("The file was saved!");
-		    }
-		}); 	
-        serialPort.close(function(){
-            console.log("CONSOLE PORT CLOSED - UPLOAD NOW");
-            console.log("Re-opening port in 45 seconds...");
-            sleep(45000);
-            serialInit();
+        }
+        else if("SKETCH" in client_cmd){
+                var sketch = Object.keys(client_cmd).map(function(key){return client_cmd[key];});
+                console.log("Sketch Recieved:");
+                console.log(sketch);
+                var fs = require('fs');
+                fs.writeFile("/tmp/test", sketch , function(err) {
+                        if(err) {
+                                console.log(err);
+                    } else {
+                        console.log("The file was saved!");
+                    }
+                });        
+                serialPort.close(function(){
+                    console.log("CONSOLE PORT CLOSED - UPLOAD NOW");
+                    console.log("Re-opening port in 45 seconds...");
+                    sleep(45000);
+                    serialInit();
+                });      
+        }
         });
-	}
-	});
 });
-
